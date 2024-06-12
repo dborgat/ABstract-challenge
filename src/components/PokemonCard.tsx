48a7f099f4fe27539ac39622fbfe7fff852483bc
dborgat <dborgat@gmail.com>
2024-06-12 12:24:43 -0300

    feat: Agregar import de tipos en PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import { RootObject } from '@/types';
 import {
   Stack,
   Text,
@@ -5,29 +6,30 @@ import {
   HStack,
   Badge,
   AspectRatio,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-export default function PokemonCard({ pokemon }) {
+export default function PokemonCard({ pokemon }: { pokemon: RootObject }) {
   return (
     <Stack
-      spacing="5"
-      boxShadow="xl"
-      p="5"
-      w="full"
-      borderRadius="xl"
-      alignItems="center"
+      spacing='5'
+      boxShadow='xl'
+      p='5'
+      w='full'
+      borderRadius='xl'
+      alignItems='center'
     >
-      <AspectRatio w="full" ratio={1}>
+      <AspectRatio w='full' ratio={1}>
         <Image
+          alt='pokemon image'
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`}
         />
       </AspectRatio>
-      <Text textAlign="center" textTransform="Capitalize">
+      <Text as='b' align='center' _firstLetter={{ textTransform: 'uppercase' }}>
         {pokemon.name}
       </Text>
       <HStack>
         {pokemon?.types.map((type) => (
-          <Badge size="xs" key={type.slot}>
+          <Badge size='xs' key={type.slot}>
             {type.type.name}
           </Badge>
         ))}
