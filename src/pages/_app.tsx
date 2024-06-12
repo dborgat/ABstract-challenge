382de881fffbdd6fee87b34aa0bf6c0e92dd827e
dborgat <dborgat@gmail.com>
2024-06-12 00:00:43 -0300

    chore: agrego PokemonData component y update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@
 import * as React from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
