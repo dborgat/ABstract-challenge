53efd13dd5735239d930cd211e5d28e6739947a9
dborgat <dborgat@gmail.com>
2024-06-13 13:41:10 -0300

    feat: Eliminar archivo Context.ts y actualizar constantes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+export const IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home"
+export const infoRequired = ['weight', 'height'] as const;
+
